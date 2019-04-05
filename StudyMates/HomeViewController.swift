27bfc4ecd0c34870056cbb62744fc3a45b62f5b5
kennethli36 <likenneth36@gmail.com>
2019-04-05 19:43:37 -0400

    Storyboard

---
+++ StudyMates/HomeViewController.swift
@@ -0,0 +1,47 @@
+//
+//  HomeViewController.swift
+//  StudyMates
+//
+//  Created by Kenneth Li on 4/5/19.
+//  Copyright © 2019 Kenneth Li. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        profileImage.layer.borderWidth = 1
+        profileImage.layer.masksToBounds = false
+        profileImage.layer.borderColor = UIColor.black.cgColor
+        profileImage.layer.cornerRadius = profileImage.frame.height/2
+        profileImage.clipsToBounds = true
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
